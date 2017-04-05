f1f6ddb5cc656a825613ceaea4efd55701123050
b03902084 <b03902084@linux6.csie.ntu.edu.tw>
2017-04-05 14:52:32 +0800

    minor change

--- hw1/src/okapi.py
+++ hw1/src/okapi.py
@@ -13,15 +13,12 @@ def read_model(model_dir):
     with open(model_dir+'/vocab.all') as fp:
         encode = fp.readline()
         vl = [line.strip() for line in fp]
-    vocab = dict(zip(vl, range(1, len(vl))))
+    vocab = dict(zip(vl, range(1, len(vl)+1)))
     return file_list, vocab
-
-def dd():
-    return defaultdict(int)
 
 def cal_tf_idf(model_dir, N):
     idf = defaultdict(int)
-    tf = defaultdict(dd)
+    tf = defaultdict(lambda: defaultdict(dict))
     doc_len = defaultdict(int)
     cur_term = -1
     with open(model_dir+'/inverted-index') as fp:
@@ -31,7 +28,7 @@ def cal_tf_idf(model_dir, N):
             if len(tmp) == 3:
                 cur_term = tmp[0] if tmp[1] == '-1' else tmp[0]+':'+tmp[1]
                 df = int(tmp[2])
-                idf[cur_term] = log((N-df+0.5)/df+0.5)
+                idf[cur_term] = log((N-df+0.5)/(df+0.5))
             elif len(tmp) == 2:
                 tf[int(tmp[0])][cur_term] = int(tmp[1])
                 #if ':' not in cur_term:
@@ -47,12 +44,14 @@ def parse(query_file, vocab):
         d = defaultdict(int)
         for term in bigram(topic.find('concepts').text.strip('。\n ').split('、'), vocab):
             d[term] += 1
+            '''
         for term in bigram(topic.find('title').text.strip('。\n ').split('、'), vocab):
             d[term] += 1
         for term in bigram(topic.find('question').text.strip('。\n ').split('、'), vocab):
             d[term] += 1
         for term in bigram(topic.find('narrative').text.strip('。\n ').split('、'), vocab):
             d[term] += 1
+            '''
         query[num] = d
     return query
 
@@ -76,14 +75,24 @@ def get_args():
     parser.add_argument('-k1', type=float)
     parser.add_argument('-k3', type=float)
     parser.add_argument('-b', type=float)
+    parser.add_argument('-doc', type=int)
     args = parser.parse_args()
-    if(args.r):
-        pass
     return args
+
+def cal_result(query_vec, query_sum, doc_vec, doc_sum):
+    result = defaultdict(lambda: defaultdict(int))
+    for doc in doc_vec:
+        for q in query_vec:
+            for term in query_vec[q]:
+                if term in doc_vec[doc]:
+                    result[q][doc] += query_vec[q][term] * doc_vec[doc][term]
+            #result[q][doc] /= query_sum[q] * doc_sum[doc]
+    return result
 
 def main():
     args = get_args()
     file_list, vocab = read_model(args.model_dir)
+    query = parse(args.query_file, vocab)
     tf, idf, doc_len, ave = cal_tf_idf(args.model_dir, len(file_list))
     query = parse(args.query_file, vocab)
 
@@ -95,25 +104,37 @@ def main():
     doc_sum = defaultdict(int)
     for doc in tf:
         for term in tf[doc]:
-            doc_vec[doc][term] = ((k1+1)*tf[doc][term]/(k1*((1-b)+b*(doc_len[doc]/ave))+tf[doc][term])) * idf[term]
+            doc_vec[doc][term] = (((k1+1)*tf[doc][term])/(k1*((1-b)+(b*(doc_len[doc]/ave)))+tf[doc][term])) * idf[term]
             doc_sum[doc] += doc_vec[doc][term]**2
         doc_sum[doc] = sqrt(doc_sum[doc])
     query_vec = defaultdict(lambda: defaultdict(int))
     query_sum = defaultdict(int)
     for q in query:
         for term in query[q]:
-            query_vec[q][term] = (((k3+1)*query[q][term])/(k3+query[q][term])) * idf[term]
+            query_vec[q][term] = (((k3+1)*query[q][term])/(k3+query[q][term]))
             query_sum[q] += query_vec[q][term]**2
         query_sum[q] = sqrt(query_sum[q])
 
-    result = defaultdict(lambda: defaultdict(int))
-    for doc in tf:
-        for q in query:
-            for term in query[q]:
-                if term in idf:
-                    result[q][doc] += query_vec[q][term] * doc_vec[doc][term]
-            result[q][doc] /= query_sum[q] * doc_sum[doc]
+    result = cal_result(query_vec, query_sum, doc_vec, doc_sum)
 
+    if args.r:
+        b = 0.8
+        d = args.doc
+        for q in query:
+            rel_doc = list(zip(*sorted(result[q].items(), key=lambda x: x[1], reverse=True)))[0][:d]
+            length = 0
+            for doc in rel_doc:
+                for term in doc_vec[doc]:
+                    length += doc_vec[doc][term]**2
+            length = sqrt(length)
+            for doc in rel_doc:
+                for term in doc_vec[doc]:
+                    query_vec[q][term] += b * (doc_vec[doc][term]/length)
+            query_sum[q] = 0
+            for term in query_vec[q]:
+                query_sum[q] += query_vec[q][term]**2
+            query_sum[q] = sqrt(query_sum[q])
+        result = cal_result(query_vec, query_sum, doc_vec, doc_sum)
 
     print('query_id,retrieved_docs')
     for qnum in result:
